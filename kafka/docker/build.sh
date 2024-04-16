caef23758406dc06b1651b0e39078f55945e2e6c
Gonçalo Silva <goncalolsilva@ua.pt>
2024-04-16 22:21:51 +0100

    updated dockerfiles, with base image and re-estructuring

--- kafka/docker/build.sh
+++ kafka/docker/build.sh
@@ -1,15 +1,30 @@
+#!/bin/sh
+
+# Variables
+baseKafkaStream="kafka-streams_base-application"
+applicationsStream="plate-recognizer_stream"
+
 # Remove previous kafka stream application image
-#docker image rm -f plate-recognizer_stream:1.0
+#docker image rm -f $baseKafkaStream:1.0
 
 # Build and export the Kafka Streams application images
 cd kafka-streams
-docker build -t plate-recognizer_stream:1.0 --no-cache=true .
+docker build -t $baseKafkaStream:1.0 .
 if [ $? -ne 0 ]; then
-    echo "Error occurred while building the Kafka Streams application image"
+    echo "Error occurred while building the Kafka Streams $baseKafkaStream image"
     exit 1
 fi
 
+# Build and export the Plate Recognizer Kafka Streams application images
+cd applications
+#docker image rm -f $applicationsStream:1.0
+
+docker build -t $applicationsStream:1.0 .
+if [ $? -ne 0 ]; then
+    echo "Error occurred while building the Kafka Streams $applicationsStream image"
+    exit 1
+fi
 
 # Start the docker-compose
-#cd ..
-#docker-compose up 
+cd ../../
+docker-compose up 
