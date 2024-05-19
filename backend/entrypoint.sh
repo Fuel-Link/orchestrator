83ad7d5890ce545f9bf854de9fd4ee5bf1e8e8ca
CatarinaBarroqueiro <95221731+CatarinaBarroqueiro@users.noreply.github.com>
2024-05-19 17:40:44 +0100

    Receiving from Kafka

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-# Wait for kafka to be ready
-cub kafka-ready -b kafka:29092 1 20
-
-# Wait for all topics to be created
-./check_topics.sh
+sleep 30
 
 # Execute your application
 npm run start:dev
--- backend/src/kafka-communication/kafka-communication.entity.ts
+++ backend/src/kafka-communication/kafka-communication.entity.ts
@@ -1,10 +1,14 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { VehicleInfo } from 'src/vehicle-info/vehicle-info.entity';
+import { Entity, Column, PrimaryGeneratedColumn, ManyToOne, JoinColumn } from 'typeorm';
 @Entity()
  export class KafkaCommunication {
  @PrimaryGeneratedColumn()
  id: number;
-@Column()
+
+ @ManyToOne(()=> VehicleInfo)
+ @JoinColumn({name: 'plate'})
  plate: string;
+ 
  @Column()
- authorization: string;
+ thingId: string;
  }
