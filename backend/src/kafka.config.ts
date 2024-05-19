6d101914f5747805b6c7eee42a489ab322fb1523
CatarinaBarroqueiro <95221731+CatarinaBarroqueiro@users.noreply.github.com>
2024-05-19 11:57:32 +0100

    combined docker compose

--- backend/src/kafka.config.ts
+++ backend/src/kafka.config.ts
@@ -1,12 +1,12 @@
 import { Kafka, logLevel } from 'kafkajs';
 const kafka = new Kafka({
- clientId: 'fuel-link',
- ssl: true,
- brokers: ["172.21.0.6:8082",
-            "172.21.0.6:8082",
-            "172.21.0.6:8082"
+ clientId: 'hello-world',
+ brokers:[ 
+ "orchestrator-kafka-rest-api-1:8082",
+ "orchestrator-kafka-rest-api-1:8082",
+ "orchestrator-kafka-rest-api-1:8082",
 ], 
- logLevel: logLevel.ERROR, 
+logLevel: logLevel.ERROR, 
 });
 export const kafkaProducer = kafka.producer();
-export const kafkaConsumer = kafka.consumer({ groupId: 'streams-wordcount-output' });
+export const kafkaConsumer = kafka.consumer({ groupId: 'hello-world' });
